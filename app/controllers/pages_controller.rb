5f0a55ab4c374ccc4168db578841e0b7c2fd119f
tylteng <teng.ty4@gmail.com>
2017-11-21 15:34:50 -0500

    implemented instance variable from layout in the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "This is the welcome page"
     render :welcome
   end
 
   def about
+    @header = "This is the about page"
     render :about
   end
 
   def contest
+    @header = "This is the contest page"
     render :contest
   end
 
