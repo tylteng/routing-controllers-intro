7d1a95cecabc98b1da1bbefa6147606284ad878a
tylteng <teng.ty4@gmail.com>
2017-11-21 16:33:25 -0500

    created route helpers for my routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/welcome' => 'pages#welcome'
-  get '/' => 'pages#welcome'
+  root 'pages#welcome'
 
   get '/about' => 'pages#about'
 
   get '/contest' => 'pages#contest'
 
-  get '/kitten/:size' => 'pages#kitten'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
 
-  get '/kittens/:size' => 'pages#kittens'
+  get '/kittens/:size' => 'pages#kittens', as: 'kittens'
 
   get '/secrets/:magic_word' => 'pages#secrets'
 end
